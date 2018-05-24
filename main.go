0cff640a53f83662d763d43edc64ec0348c24c6a
vvmk <vincentmasiello@gmail.com>
2018-05-24 16:23:47 -0400

    Add some routes

--- main.go
+++ main.go
@@ -5,12 +5,40 @@ import (
 	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "bounce, %q", html.EscapeString(r.URL.Path))
-	})
+const port = ":8080"
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	router.HandleFunc("/ssrroutine/routines/{routineId}", HandleGetRoutine)
+	router.HandleFunc("/ssrroutine/library/{userId}", HandleGetLibrary)
+	router.HandleFunc("/ssruser/users/{userId}", HandleGetUser)
+
+	log.Fatal(http.ListenAndServe(port, router))
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "ping, %q", html.EscapeString(r.URL.Path))
+}
+
+func HandleGetRoutine(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	routineId := vars["routineId"]
+	fmt.Fprintf(w, "{ Routine %q }", routineId)
+}
+
+func HandleGetLibrary(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	userId := vars["userId"]
+	fmt.Fprintf(w, "{ Library for user: %q }", userId)
+}
+
+func HandleGetUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	userId := vars["userId"]
+	fmt.Fprintf(w, "{ User with id: %q }", userId)
 }
