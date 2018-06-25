680a868295074e35008e9b93888676a882a3e06b
vvmk <vincentmasiello@gmail.com>
2018-06-24 22:33:32 -0400

    Add temporary gorilla/sessions demo

--- recoverer.go
+++ recoverer.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func recoverHandler(next http.Handler) http.Handler {
+func Recoverer(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
