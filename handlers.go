319e8c4cb91220e6dafe60c73f8b583de3aee36c
vvmk <vincentmasiello@gmail.com>
2018-06-29 00:45:33 -0400

    Rewrite URIs, stub missing handlers

--- handlers.go
+++ handlers.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"html"
 	"net/http"
 	"strconv"
 
@@ -34,69 +33,6 @@ type UserRepo interface {
 
 const JSON = "application/json; charset=UTF-8"
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "ping, %q", html.EscapeString(r.URL.Path))
-}
-
-func GetRoutine(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-
-	routineId, err := strconv.Atoi(vars["routineId"])
-	if err != nil {
-		panic(err)
-	}
-
-	routine := RepoFindRoutine(routineId)
-
-	w.Header().Set("Content-Type", JSON)
-	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(routine); err != nil {
-		panic(err)
-	}
-}
-
-func GetLibrary(w http.ResponseWriter, r *http.Request) {
-
-	user := r.Context().Value("user")
-	fmt.Fprintf(w, "this is an authenticated request")
-	fmt.Fprintf(w, "Claim content:\n")
-	for k, v := range user.(*jwt.Token).Claims.(jwt.MapClaims) {
-		fmt.Fprintf(w, "%s : \t%#v\n", k, v)
-	}
-
-	vars := mux.Vars(r)
-
-	userId, err := strconv.Atoi(vars["userId"])
-	if err != nil {
-		panic(err)
-	}
-
-	library := RepoFindLibrary(userId)
-
-	w.Header().Set("Content-Type", JSON)
-	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(library); err != nil {
-		panic(err)
-	}
-}
-
-func GetUser(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-
-	userId, err := strconv.Atoi(vars["userId"])
-	if err != nil {
-		panic(err)
-	}
-
-	user := RepoFindUser(userId)
-
-	w.Header().Set("Content-Type", JSON)
-	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(user); err != nil {
-		panic(err)
-	}
-}
-
 func Login(w http.ResponseWriter, r *http.Request) {
 	creds := map[string]string{"vvmk": "secret"}
 
@@ -125,4 +61,84 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	} else {
 		w.WriteHeader(http.StatusUnauthorized)
 	}
+}
+
+func GetLibrary(w http.ResponseWriter, r *http.Request) {
+
+	user := r.Context().Value("user")
+	fmt.Fprintf(w, "this is an authenticated request")
+	fmt.Fprintf(w, "Claim content:\n")
+	for k, v := range user.(*jwt.Token).Claims.(jwt.MapClaims) {
+		fmt.Fprintf(w, "%s : \t%#v\n", k, v)
+	}
+
+	vars := mux.Vars(r)
+
+	userId, err := strconv.Atoi(vars["userId"])
+	if err != nil {
+		panic(err)
+	}
+
+	library := RepoFindLibrary(userId)
+
+	w.Header().Set("Content-Type", JSON)
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(library); err != nil {
+		panic(err)
+	}
+}
+
+func GetRoutine(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	routineId, err := strconv.Atoi(vars["routineId"])
+	if err != nil {
+		panic(err)
+	}
+
+	routine := RepoFindRoutine(routineId)
+
+	w.Header().Set("Content-Type", JSON)
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(routine); err != nil {
+		panic(err)
+	}
+}
+
+func CreateRoutine(w http.ResponseWriter, r *http.Request) {
+}
+
+func ForkRoutine(w http.ResponseWriter, r *http.Request) {
+}
+
+func EditRoutine(w http.ResponseWriter, r *http.Request) {
+}
+
+func DeleteRoutine(w http.ResponseWriter, r *http.Request) {
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	userId, err := strconv.Atoi(vars["userId"])
+	if err != nil {
+		panic(err)
+	}
+
+	user := RepoFindUser(userId)
+
+	w.Header().Set("Content-Type", JSON)
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(user); err != nil {
+		panic(err)
+	}
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+}
+
+func EditUser(w http.ResponseWriter, r *http.Request) {
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
 }
