85be6a7c127c9c11df43101e7795445fb49b6954
vvmk <vincentmasiello@gmail.com>
2018-07-02 23:51:39 -0400

    generate a token for email confirm/pass resets

--- crypt.go
+++ crypt.go
@@ -1,13 +1,33 @@
 package main
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"crypto/rand"
+	"encoding/base64"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
 }
 
-func CheckPasswordHash(password, hash string) bool {
+func CheckPasswordHash(password string, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
+}
+
+// GenerateEmailToken creates a base64'd 16B crypto-random for single-use email auth
+func GenerateEmailToken() (string, err) {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+	token, err := base64.URLEncoding.EncodeToString(b)
+	if err != nil {
+		return "", err
+	}
+
+	return token
 }
