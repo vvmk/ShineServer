445b6e4f99d294524264f6e9f4cdd572b0f38f77
vvmk <vincentmasiello@gmail.com>
2018-07-03 21:22:25 -0400

    Fix routine column ordering in queries

--- models/routines.go
+++ models/routines.go
@@ -28,7 +28,8 @@ func (db *DB) FindRoutineById(routineId int) (*Routine, error) {
 	query := `SELECT * FROM routines WHERE routine_id=$1;`
 
 	var d []byte
-	err := db.QueryRow(query, routineId).Scan(&r.RoutineId, &r.Title, &r.TotalDuration, &r.Character, &r.OriginalCreatorId, &r.Created, &r.Popularity, &d, &r.OriginalCreatorId)
+	err := db.QueryRow(query, routineId).Scan(&r.RoutineId, &r.Title, &r.TotalDuration, &r.Character, &r.CreatorId, &r.Created, &r.Popularity, &d, &r.OriginalCreatorId)
+
 	if err != nil {
 		return nil, err
 	}
@@ -56,7 +57,7 @@ func (db *DB) FindRoutinesByCreator(creatorId int) ([]*Routine, error) {
 		r := &Routine{}
 
 		var d []byte
-		err := rows.Scan(&r.RoutineId, &r.Title, &r.TotalDuration, &r.Character, &r.OriginalCreatorId, &r.CreatorId, &r.Created, &r.Popularity, &d)
+		err := rows.Scan(&r.RoutineId, &r.Title, &r.TotalDuration, &r.Character, &r.CreatorId, &r.Created, &r.Popularity, &d, &r.OriginalCreatorId)
 		if err != nil {
 			return nil, err
 		}
